a689828e563cabc532a4d2d59608a00c8820af6b
sch <sch071016@mail.ustc.edu.cn>
2026-03-14 10:22:45 +0800

    Fix an occasion that the number is negative.

--- math_utils.c
+++ math_utils.c
@@ -1,5 +1,9 @@
 #include"math_utils.h"
 int factorial(int n){
+    if(n<0){
+        printf("Error: Factorial is not defined for negative numbers.\n");
+        return -1; // Return -1 to indicate an error   
+    }
     int object=1;
     for(int i=1;i<=n;i++){
         object*=i;
